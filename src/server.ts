10fa999256b15e467878dc47f4fe8b1bc36923e8
rayan ribeiro <rayanrib2012@hotmail.com>
2025-02-27 23:46:32 -0300

    Updated port api test

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-app.listen(process.env.API_PORT, () => {
+app.listen(3000, () => {
   console.log();
-  console.log(`Server running on port ${process.env.API_PORT}`);
+  console.log(`Server running on port ${3000}`);
 });
